230395947064a9b242ba00376e7048e40140443e
shivam907 <shivamkaushal907@gmail.com>
2022-10-22 23:54:20 +0530

    Added Logout Feature

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,11 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
+  console.log(req.header("Authorization"));
   const token = req.header("Authorization").replace("Bearer ", "");
   const decoded = jwt.verify(token, "cezisbest");
   const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
+  req.token = token;
   req.user = user;
   next();
 };
