08f0fa9cf2d7dc261fcdb5ff6fedca4569874967
shivam907 <shivamkaushal907@gmail.com>
2022-10-22 17:06:41 +0530

    Added Authentication middleware to get user data

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,11 +4,7 @@ const User = require("../models/User");
 module.exports = async (req, res, next) => {
   const token = req.header("Authorization").replace("Bearer ", "");
   const decoded = jwt.verify(token, "cezisbest");
-  console.log(decoded);
   const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
-  console.log(user);
   req.user = user;
-  // res.send({ token });
-  console.log("In the middleware");
   next();
 };
